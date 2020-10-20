bfd33920fd53a9529623609a842b1dff37edc7f9
Lui Fensmark Nielsencdcd..git config --global user.email 3862142@rts-365.dk <3862142@rts-365.dk>
2020-10-20 12:54:26 +0200

    Dette er min package

---
+++ README.md
@@ -0,0 +1,11 @@
+# Rainbow colors console log
+This package makes a colorful console log.
+It make your colors into rainbow colors
+
+## Usage
+Open terminal to install and write: `npm i something@rainbow-log`
+
+```Javascript
+var rainbowLog = require(something@rainbow-log);
+
+rainbowLog("this is a rainbow colored console log");
---
+++ index.js
@@ -0,0 +1,17 @@
+function rainbowLog(string) {
+    // Nedenstående farver er regnbue farver
+    var colors = ["\x1b[31m", "\x1b[33m", "\x1b[32m", "\x1b[36m", "\x1b[34m"];
+    var reset = "\x1b[0m";
+    var output = "";
+
+
+    for (let i = 0; i < string.length; i++) {
+        output += colors[i % colors.length] + string[i]
+    }
+    console.log(output + reset);
+
+}
+
+rainbowLog("Hej Verden");
+
+module.exports = rainbowLog;
